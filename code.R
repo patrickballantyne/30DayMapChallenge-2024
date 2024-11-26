baf1022d9c988924f6b61389dc61300dafcc8a86
Ballantyne <pball24@liverpool.ac.uk>
2024-11-26 11:33:35 +0000

    updated map and code

--- code.R
+++ code.R
@@ -6,7 +6,7 @@ library(ggpubr) ## facet plotting
 
 # 1. Data -----------------------------------------------------------------
 
-## Overture points of interest data - Available at: https://data.cdrc.ac.uk/dataset/point-interest-data-united-kingdom
+## Overture points of interest data - Available at: https://data.cdrc.ac.uk/dataset/point-interest-data-united-kingdom. Too big for the repo (apologies!)
 db <- st_read("data/poi_uk/poi_uk.gpkg")
 
 ## UK local authority districts - Available at: https://geoportal.statistics.gov.uk/search?q=BDY_LAD%3BDEC_2022&sort=Title%7Ctitle%7Casc
@@ -19,8 +19,22 @@ lad_dep <- read.csv("data/LAD-Deprivation.csv")
 
 ## Get all UK deli's
 deli <- db %>%
-  filter(main_category == "delicatessen") %>%
+  filter(main_category == "delicatessen" | main_category == "sandwich_shop") %>%
   select(id, primary_name, main_category, geom)
+
+## Calculate frequency by name (to identify where the big brands are)
+deli_ls <- deli %>%
+  as.data.frame() %>%
+  select(primary_name) %>%
+  group_by(primary_name) %>%
+  add_count() %>%
+  distinct() %>%
+  arrange(desc(n))
+
+## Drop big brands
+to_drop <- c("Subway UK & Ireland", "Subway", "Pret A Manger")
+deli <- deli %>%
+  filter(!primary_name %in% to_drop)
 
 ## Tidy up the LAD data
 lad <- lad %>%
@@ -46,26 +60,35 @@ deli_count <- deli_lad %>%
 ## Attach to boundaries
 lad <- merge(lad, deli_count, by = "LAD22NM", all.x = TRUE)
 
+## Calculate area (m2) and number per km2
+lad <- lad %>%
+  mutate(n = case_when(is.na(n) ~ 0, 
+                       TRUE ~ n)) %>%
+  mutate(area_meters = st_area(lad),
+         area_kms = area_meters / 1000000) %>%
+  mutate(n_area = n / area_kms,
+         n_area = as.numeric(n_area)) %>%
+  arrange(desc(n_area)) %>%
+  select(LAD22NM, LAD22CD, n_area, geometry)
+
 ## Calculate jenks grouping
-foo <- classIntervals(lad$n, n = 5, style = "jenks")
+foo <- classIntervals(lad$n_area, n = 5, style = "quantile")
 foo$brks
 
 ## Group
 lad <- lad %>%
-  mutate(n = case_when(is.na(n) ~ 0,
-                       TRUE ~ n),
-         n_grp = case_when(n < 2 ~ "0 - 2",
-                           n >= 2 & n < 5 ~ "2 - 5",
-                           n >= 5 & n < 9 ~ "5 - 9",
-                           n >= 9 & n < 17  ~  "9 - 17",
-                           n >= 17 ~ "17+"), 
-         n_grp = factor(n_grp, levels = c("0 - 2", "2 - 5", "5 - 9", "9 - 17", "17+")))
+  mutate(n_grp = case_when(n_area < 0.005 ~ "0.000 - 0.005",
+                           n_area >= 0.005 & n_area < 0.011 ~ "0.005 - 0.011",
+                           n_area >= 0.011 & n_area < 0.032 ~ "0.011 - 0.032",
+                           n_area >= 0.032 & n_area < 0.112 ~  "0.032 - 0.112",
+                           n_area >= 0.112 ~ "0.112+")) %>%
+  mutate(n_grp = factor(n_grp, levels = c("0.000 - 0.005", "0.005 - 0.011", "0.011 - 0.032", "0.032 - 0.112", "0.112+")))
 
 ## Attach deprivation data
 lad_stat <- lad %>%
   inner_join(lad_dep, by = c("LAD22CD", "LAD22NM")) %>%
   as.data.frame() %>%
-  select(LAD22NM, n, n_grp, avgIMD)
+  select(LAD22NM, n_area, n_grp, avgIMD)
 
 # 3. Map ------------------------------------------------------------------
 
@@ -74,11 +97,11 @@ map <- ggplot(lad) +
   geom_sf(aes(fill = n_grp), color = NA) +
   scale_fill_manual(values = c("#ebe47a", "#eacc6a", "#e9c062", "#e7843a", "#e56022")) +
   labs(title = "Where the Crust Is: Mapping the UK's Deli Divide", subtitle = "Day 29 - Overture, #30DayMapChallenge",
-       fill = "Number of Delis") +
+       fill = bquote("Delis per"~km^2)) +
   theme_void() +
   theme(legend.position = "bottom", plot.title = element_text(face = "bold", size = 16),
-        plot.subtitle = element_text(size = 14), legend.text = element_text(size = 12),
-        legend.title = element_text(size = 12))
+        plot.subtitle = element_text(size = 14), legend.text = element_text(size = 10),
+        legend.title = element_text(size = 10))
 
 # 4. Graph ----------------------------------------------------------------
 
@@ -86,15 +109,17 @@ map <- ggplot(lad) +
 graph <- ggplot(lad_stat, aes(x = n_grp, y = avgIMD, fill = n_grp)) +
   geom_boxplot() +
   scale_fill_manual(values = c("#ebe47a", "#eacc6a", "#e9c062", "#e7843a", "#e56022")) +
-  labs(title = " ", subtitle = "Delis appear more frequently in more deprived parts of the UK!",
+  labs(title = "Where the Crust Is: Mapping the UK's Deli Divide", subtitle = "Delis appear more frequently in more deprived parts of the UK!",
        caption = "Visualisations by Patrick Ballantyne (@DrPatBallantyne)\nData from CDRC, Overture Maps Foundation and Office for National Statistics",
-       x = "Number of Delis", y = "Average IMD score", fill = "Delicatessen (n)") +
+       x = bquote("Delis per"~km^2), y = "Average IMD score", fill = bquote("Delis per"~km^2)) +
   theme_minimal() +
-  theme(legend.position = "none", plot.subtitle = element_text(size = 14),
+  theme(legend.position = "none", 
+        plot.title = element_text(size = 16, face = "bold"),
+        plot.subtitle = element_text(size = 14),
         axis.title = element_text(size = 12))
 
 # 5. Figure ---------------------------------------------------------------
 
 ## Arrange
 ggarrange(map,  graph)
-ggsave("Day29-Overture.jpg", dpi = 600)
+ggsave("Day29-Overture.jpg", dpi = 600, height = 8, width = 14)
